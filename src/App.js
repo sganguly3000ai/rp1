e9d26dc3d6df72ee1492d5f6289c9d381574dde4
sgcloud3000 <sgcloud3000@gmail.com>
2024-05-21 15:53:03 -0500

    Finished winner

--- src/App.js
+++ src/App.js
@@ -12,25 +12,22 @@ function Square({value, onSquareClick}) {
   );
 }
 
-function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
 
-   
-    function handleClick(i){
-      if (squares[i]) {
+  const nextSquares = squares.slice();
+
+  function handleClick(i){
+      if (calculateWinner(squares) || squares[i]) {
         return;
       }
-      
-      const nextSquares = squares.slice();
+
       if (xIsNext) {
         nextSquares[i] = "X";
       } else {
         nextSquares[i] = 'O';
       }
-      setSquares(nextSquares);
-      setXIsNext(!xIsNext);
-    }
+    onPlay(nextSquares);
+  }
 
   const winner = calculateWinner(squares);
   let status;
@@ -66,13 +63,17 @@ function Board() {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+  
+  
   function handlePlay(nextSquares) {
-    // TODO
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
   }
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
